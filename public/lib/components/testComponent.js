d5a29d57959373ad3b04c843d28456599e6be98a
valmsmith39a <valmsmith39a@gmail.com>
2016-05-11 11:37:07 -0700

    sets up test component

---
+++ public/lib/components/testComponent.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const testComponent = () => (<div>Hello from testComponent</div>)
+
+export default testComponent;
---
+++ public/lib/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TestComponent from './components/testComponent.js';
+
+ReactDOM.render(
+  <TestComponent />,
+  document.getElementById('app')
+);
