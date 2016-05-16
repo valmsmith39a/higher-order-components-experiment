659e6cdd23196fc0d835bffd111d361ca518e220
valmsmith39a <valmsmith39a@gmail.com>
2016-05-15 18:26:31 -0700

    implements constructor, state, wires up button to increment function, implements counter display

--- public/lib/components/CounterWithPropsValidationEx.js
+++ public/lib/components/CounterWithPropsValidationEx.js
@@ -1,9 +1,24 @@
 import React from 'react';
 
 export default class CounterWithPropsValidataionEx extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = { counter: 0 }
+    this.increment = this.increment.bind(this)
+  }
+
+  increment() {
+
+  }
+
   render() {
     return(
-      <div>Hello from Props Validation Ex</div>
+      <div>
+        <p>Hello from Props Validation Ex</p>
+        <button onClick={this.increment}> IncrementBtn </button>
+        <p>{this.state.counter}</p>
+      </div>
     )
   }
 }
